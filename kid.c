33bc644812d9f66246cededd0f95bba6a8bcacc3
Kienyew <aobi100bt@hotmail.com>
2020-03-07 22:41:06 +0800

    modify `kid.c`: change bg character

--- kid.c
+++ kid.c
@@ -10,8 +10,8 @@ int main() {
     init_pair(2, COLOR_RED, COLOR_WHITE);
 
     sonny = subwin(stdscr, 20, 20, 10, 10);
-    wbkgd(stdscr, COLOR_PAIR(1));
-    wbkgd(sonny, COLOR_PAIR(2));
+    wbkgd(stdscr, COLOR_PAIR(1) | ' ');
+    wbkgd(sonny, COLOR_PAIR(2) | ' ');
     waddstr(stdscr, "hello child");
     waddstr(sonny, "hi mother");
     refresh();
