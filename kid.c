b496fa86efcaa0210e0c513f6b8857cec5f7e3e2
Kienyew <aobi100bt@hotmail.com>
2020-03-07 21:36:41 +0800

    add `kid.c`: subwindow

---
+++ kid.c
@@ -0,0 +1,24 @@
+#include <ncurses.h>
+#include <stdio.h>
+
+int main() {
+    WINDOW* sonny;
+    initscr();
+
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_YELLOW);
+    init_pair(2, COLOR_RED, COLOR_WHITE);
+
+    sonny = subwin(stdscr, 20, 20, 10, 10);
+    wbkgd(stdscr, COLOR_PAIR(1));
+    wbkgd(sonny, COLOR_PAIR(2));
+    waddstr(stdscr, "hello child");
+    waddstr(sonny, "hi mother");
+    refresh();
+    getch();
+
+
+
+    endwin();
+    return 0;
+}
